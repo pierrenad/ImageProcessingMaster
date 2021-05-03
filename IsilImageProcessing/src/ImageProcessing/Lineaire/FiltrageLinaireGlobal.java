e861eb712e306cf83524d0f41739e65aaae2cc79
pierre <pierre.nadin@student.hepl.be>
2021-05-03 17:07:59 +0200

    Create classes for step 1
    Need to be implemented

---
+++ IsilImageProcessing/src/ImageProcessing/Lineaire/FiltrageLinaireGlobal.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ImageProcessing.Lineaire;
+
+/**
+ *
+ * @author Pierre
+ */
+public class FiltrageLinaireGlobal {
+
+    public static int[][] filtrePasseBasIdeal(int[][] image, int frequenceCoupure) {
+
+        return null;
+    }
+
+    public static int[][] filtrePasseHautIdeal(int[][] image, int frequenceCoupure) {
+
+        return null;
+    }
+
+    public static int[][] filtrePasseBasButterworth(int[][] image, int frequenceCoupure, int ordre) {
+
+        return null;
+    }
+
+    public static int[][] filtrePasseHautButterworth(int[][] image, int frequenceCoupure, int ordre) {
+
+        return null;
+    }
+}
---
+++ IsilImageProcessing/src/ImageProcessing/Lineaire/FiltrageLineaireLocal.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ImageProcessing.Lineaire;
+
+/**
+ *
+ * @author Pierre
+ */
+public class FiltrageLineaireLocal {
+
+    public static int[][] filtreMasqueConvolution(int[][] image, double[][] masque) {
+
+        return null;
+    }
+
+    public static int[][] filtreMoyenneur(int[][] image, int tailleMasque) {
+
+        return null; 
+    }
+}
